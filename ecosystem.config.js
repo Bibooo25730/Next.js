fbb233a3c901cee5a0c7ac6a1ceb0e88448dad3e
Bibooo25730 <2573018577@qq.com>
2023-06-09 19:32:53 +0800

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,7 @@ module.exports = {
   }],
   deploy : {
     production : {
-      key  : '',
-      user : 'Bibooo', //SSH_USERNAME
-      host : '162.14.108.172',
-      ref  : 'origin/main',
-      repo : 'git@github.com:Bibooo25730/Next.js.git',
-      path : '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy' : 'sourch ~/.nvm/nvm.sh &&  npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
+
     }
   }
 };
